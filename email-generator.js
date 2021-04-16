d6df57e0d49af3d1fb31c0430a6cafe4f5bfd4c3
Ahmed (Rick) Abduljabbar <ahmed.m.abduljabbar@outlook.com>
2021-04-16 09:27:20 -0400

    Setup basic HTML & JS

---
+++ email-generator.js
@@ -0,0 +1,18 @@
+var inText = document.getElementById("inText");
+var inGen = document.getElementById("inGen");
+var outText = document.getElementById("outText");
+//called when clicking 'Generate Fields' button
+//reads text and generates input Boxes inside the 'inGen' div
+function genFlds(){
+
+}
+//called when clicking 'Generate Output' button
+//generates output in outText
+function genOut(){
+
+}
+//called when clicking 'Send E-mail' button
+//sends an email using generated output to address in 'mail' feild
+function mail(){
+
+}
