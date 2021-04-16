c2a2c8a352a76595cec4ad60b84cfbd2d78ca010
Ahmed (Rick) Abduljabbar <ahmed.m.abduljabbar@outlook.com>
2021-04-16 10:19:20 -0400

    Field detection done.

--- email-generator.js
+++ email-generator.js
@@ -1,10 +1,46 @@
 var inText = document.getElementById("inText");
 var inGen = document.getElementById("inGen");
 var outText = document.getElementById("outText");
+//Holds output
+var out = "";
+//Holds the entered fields
+var fields = []
 //called when clicking 'Generate Fields' button
 //reads text and generates input Boxes inside the 'inGen' div
 function genFlds(){
+	var text = inText.value;
+	fields = [];
+	readingFld = false;
+	fld = ""; 
+	for (i = 0; i < text.length;i++){
+		//if reading
+		if (readingFld){
+			//check for ']', ignore '\]'
+			if (text[i] == ']'){
+				//ignoring '\]'
+				if (i > 1)
+					if (text[i-1] =='\\'){
+						fld += text[i];
+						continue;
+					}
+				fields.push(fld);
+				fld = "";
+				readingFld = false;
+				continue;
+			}
+			fld += text[i];
+		}
+		//check for '[', ignore '\['
+		if (text[i] == '['){
+			//ignoring '\['
+			if (i > 1)
+				if (text[i-1] =='\\')
+					continue;
+			readingFld = true;
+		}
 
+	}
+	console.log(fields)
 }
 //called when clicking 'Generate Output' button
 //generates output in outText
